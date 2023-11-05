58cc3b748767d1ab5eab40872b9b515fbfd67b08
Sitnikov Dmitry <betani.elvin@mail.ru>
2023-11-05 10:30:05 +0300

    Feat: create sign in & sign up routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json("Пользователь успешно создан!");
+  } catch (err) {
+    // next(errorHandler(550, 'Такой пользователь уже существует'))
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser)
+      return next(errorHandler(404, "Такой пользователь не найден!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword)
+      return next(errorHandler(401, "Неправильные учетные данные!"));
+
+    const token = jwt.sign({ id: validUser._id }, "dsfsafdfgsdfgsdf34f");
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(err);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,36 @@
-import express  from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
-const app = express()
+mongoose
+  .connect(
+    "mongodb+srv://sahand:sahand@mern-estate.szepsdq.mongodb.net/mern-estate?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connect MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json())
+
 app.listen(3000, () => {
-	console.log('Server running 3000')
+  console.log("Server running 3000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || 'Ошибка на сервере'
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	})
 })
