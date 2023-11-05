8baafa3542f8524e192d1f69159c0bbe9bc78d15
Sitnikov Dmitry <betani.elvin@mail.ru>
2023-11-05 16:02:47 +0300

    Feat: create user update api

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { HiOutlineUserCircle } from "react-icons/hi2";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
@@ -22,7 +26,7 @@ export default function Header() {
           />
           <FaSearch className="text-slate-600" />
         </form>
-        <ul className="flex gap-4 ">
+        <ul className="flex gap-4 items-center">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:bg-slate-300 transition px-[0.45rem] pb-[0.15rem] cursor-pointer rounded-md">
               На главную
@@ -33,10 +37,17 @@ export default function Header() {
               О нас
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-slate-700 hover:bg-slate-300 transition px-[0.45rem] pb-[0.15rem] cursor-pointer rounded-md">
-              Войти
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <HiOutlineUserCircle
+                size={24}
+                className="text-slate-600 hover:text-slate-700 transition mt-[0.1rem]"
+              />
+            ) : (
+              <li className="text-slate-700 hover:bg-slate-300 transition px-[0.45rem] pb-[0.15rem] cursor-pointer rounded-md">
+                Войти
+              </li>
+            )}
           </Link>
         </ul>
       </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { LiaUserCogSolid } from "react-icons/lia";
 
 export default function Profile() {
   return (
-	<div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <div className="flex items-center justify-center mt-7">
+        <LiaUserCogSolid className="text-slate-500 text-7xl " />
+      </div>
+      <form className="flex flex-col gap-4 mt-9">
+        <input
+          type="text"
+          placeholder="Имя пользователя"
+          className="border p-3 rounded-lg focus:outline-slate-500"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg focus:outline-slate-500"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          className="border p-3 rounded-lg focus:outline-slate-500"
+          id="password"
+        />
+        <button className=" mt-4 bg-slate-500 text-white p-3 rounded-lg uppercase hover:bg-slate-600 hover:shadow-lg transition min-w-[15rem] m-auto disabled:opacity-80 disabled:hover:bg-slate-500 disabled:hover:opacity-80 disabled:hover:shadow-none">
+          Обновить
+        </button>
+      </form>
+      <div className='flex justify-end mt-7 gap-5'>
+        <span className='text-red-700 hover:text-red-900 transition cursor-pointer'>Удалить аккаунт</span>
+        <span className='text-red-700 hover:text-red-900 transition cursor-pointer'>Выйти</span>
+      </div>
+    </div>
+  );
 }
