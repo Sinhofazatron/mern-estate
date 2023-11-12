3c27a424ccd18525cb08e6e53e68bbf1fdd27908
Sitnikov Dmitry <betani.elvin@mail.ru>
2023-11-12 17:07:40 +0300

    Feat: complete create listing page

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDf4ccfmyUc-DIHtkF1JrgwNdaxai3VdZg",
+  authDomain: "mern-estate-77420.firebaseapp.com",
+  projectId: "mern-estate-77420",
+  storageBucket: "mern-estate-77420.appspot.com",
+  messagingSenderId: "610396292828",
+  appId: "1:610396292828:web:581b462ca99e5e5f305619",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
